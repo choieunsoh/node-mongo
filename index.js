0b38fd8d87d8e5c8fa8022fdeb5dfa0130634a5f
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-01-26 13:41:52 +0700

    replace callback hell

--- index.js
+++ index.js
@@ -6,42 +6,43 @@ const dbOps = require("./operations");
 const url = process.env.MONGO_DB || `mongodb://localhost:27017/`;
 const dbname = `conFusion`;
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null);
-  console.log(`Connected correctly to server.`);
+MongoClient.connect(url)
+  .then((client) => {
+    console.log(`Connected correctly to server.`);
 
-  const db = client.db(dbname);
-  const collName = "dishes";
-  dbOps.insertDocument(
-    db,
-    { name: "Pizza", description: "Test Pizza" },
-    collName,
-    (result) => {
-      console.log("Insert document\n", result);
-
-      dbOps.findDocuments(db, collName, (documents) => {
+    const db = client.db(dbname);
+    const collName = "dishes";
+    const pizza = { name: "Seafood Pizza", description: "Test Pizza" };
+    dbOps
+      .insertDocument(db, pizza, collName)
+      .then((result) => {
+        console.log("Insert document\n", result);
+        return dbOps.findDocuments(db, collName);
+      })
+      .then((documents) => {
         console.log("Found documents\n", documents);
-
-        dbOps.updateDocument(
+        return dbOps.updateDocument(
           db,
-          { name: "Pizza" },
-          { description: "Updated desc" },
-          collName,
-          (result) => {
-            console.log("Update document\n", result);
-
-            dbOps.findDocuments(db, collName, (documents) => {
-              console.log("Found documents\n", documents);
-
-              db.dropCollection(collName, (result) => {
-                console.log("Drop collection\n", result);
-
-                client.close();
-              });
-            });
-          }
+          pizza,
+          { description: "Updated Pizza" },
+          collName
         );
-      });
-    }
-  );
-});
+      })
+      .then((result) => {
+        console.log("Update document\n", result);
+        return dbOps.findDocuments(db, collName);
+      })
+      .then((documents) => {
+        console.log("Found documents\n", documents);
+        return db.dropCollection(collName);
+      })
+      .then((result) => {
+        console.log("Drop collection\n", result);
+        return client.close();
+      })
+      .then(() => {
+        console.log("Connection closed.");
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
